b3c4a2d7591e312d38f6568c780f45a34a62d063
Terry Welch <terrywelch30625@gmail.com>
2022-08-09 15:47:27 +0000

    trafficlight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,45 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [ selectedColor, setSelectedColor] = useState("");
-	return (
-		<div className="trafficLight">
-			<div className="cable"></div>
-			<div className="container-fluid">
-				<div
-					onClick={() => setSelectedColor("red")}
-					className={"light red"+((selectedColor === "red") ? "light red redglow" : "light red")
-				}></div>
-				<div
-					onClick={() => setSelectedColor("yellow")}
-					className={"light yellow"+((selectedColor === "yellow") ? "light yellow yellowglow" : "light yellow")
-				}></div>
-				<div
-					onClick={() => setSelectedColor("green")}
-					className={"light green"+((selectedColor === "green") ? "light green greenglow" : "light green")
-				}></div>
-			</div>
-		</div>
-	);
+  const [selectedColor, setSelectedColor] = useState("");
+  return (
+    
+      <div className="trafficLight">
+        <div className="cable mx-auto"></div>
+        <div className="container light-container mx-auto pt-2">
+          <div
+            onClick={() => setSelectedColor("red")}
+            className={
+              " mx-auto light red" +
+              (selectedColor === "red" ? "light red redglow" : "light red")
+            }
+          ></div>
+          <div
+            onClick={() => setSelectedColor("yellow")}
+            className={
+              "mx-auto light yellow" +
+              (selectedColor === "yellow"
+                ? "light yellow yellowglow"
+                : "light yellow")
+            }
+          ></div>
+          <div
+            onClick={() => setSelectedColor("green")}
+            className={
+              "mx-auto light green" +
+              (selectedColor === "green"
+                ? "light green greenglow"
+                : "light green")
+            }
+          ></div>
+        </div>
+      </div>
+   
+  );
 };
 
 export default Home;
